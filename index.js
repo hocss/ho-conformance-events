45998495312f2b3e5cb6e12520ae4806fd834995
mattstyles <matt@veryfizzyjelly.com>
2015-04-15 22:04:48 +0100

    #update simplify model

--- index.js
+++ index.js
@@ -3,19 +3,11 @@
  * Parse event constants
  */
 exports.PARSE = {
-    NEW_ROOT: 'new:root',
-
+    ROOT: 'object:root',
     IMPORT: 'object:import',
-
     RULE: 'object:rule',
-    NEW_RULE: 'new:rule',
-
     RULESET: 'object:ruleset',
-    NEW_RULESET: 'new:ruleset',
-
     VARIABLE: 'object:variable',
-    NEW_VARIABLE: 'new:variable',
-
     SELECTOR: 'object:selector',
-    NEW_SELECTOR: 'new:selector'
+    SELECTORS: 'object:selectors'
 }
