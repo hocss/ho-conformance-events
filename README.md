b79c270ed1bf188f4dc0974fd2ea0dcd6fca4542
mattstyles <matt@veryfizzyjelly.com>
2015-04-15 21:39:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ho-conformance-events
+
+Holds common event constants for writing conformance tasks for ho.
+
+```
+npm i -S ho-conformance-events
+```
+
+```
+import ho from 'ho-conformance-events/lib'
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+
+/**
+ * Parse event constants
+ */
+exports.PARSE = {
+    NEW_ROOT: 'new:root',
+
+    IMPORT: 'object:import',
+
+    RULE: 'object:rule',
+    NEW_RULE: 'new:rule',
+
+    RULESET: 'object:ruleset',
+    NEW_RULESET: 'new:ruleset',
+
+    VARIABLE: 'object:variable',
+    NEW_VARIABLE: 'new:variable',
+
+    SELECTOR: 'object:selector',
+    NEW_SELECTOR: 'new:selector'
+}
