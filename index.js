b863eb484f72ad0ac196dd785105a35fc2bac815
Matt Styles <matt@veryfizzyjelly.com>
2015-04-16 11:09:59 +0100

    #add comments and mixins

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
  * Parse event constants
  */
 exports.PARSE = {
-    ROOT: 'object:root',
-    IMPORT: 'object:import',
-    RULE: 'object:rule',
-    RULESET: 'object:ruleset',
-    VARIABLE: 'object:variable',
-    SELECTOR: 'object:selector',
-    SELECTORS: 'object:selectors'
+    ROOT: 'parse:root',
+    IMPORT: 'parse:import',
+    RULE: 'parse:rule',
+    RULESET: 'parse:ruleset',
+    VARIABLE: 'parse:variable',
+    SELECTOR: 'parse:selector',
+    SELECTORS: 'parse:selectors',
+    COMMENT: 'parse:comment',
+    MIXIN: 'parse:mixin',
+    MIXIN_DEFINITION: 'parse:mixinDefintion',
+    MIXIN_CALL: 'parse:mixinCall'
 }
